cfab156e2495e9415575f001e5f1fb8044f4d32b
gabrielibus <gabo.lazos@gmail.com>
2021-08-20 11:40:28 -0500

    adding recursive script

--- script.py
+++ script.py
@@ -2,8 +2,10 @@ from PIL import Image
 import pytesseract
 pytesseract.pytesseract.tesseract_cmd=r'C:\Program Files\Tesseract-OCR\tesseract.exe'
 
-image = Image.open('./example.jpeg')
+file = input('Por favor arrastre la imagen a esta ventana para procesarla: ')
+file = (file.split('"')[1])
+image = Image.open(file)
 texto = pytesseract.image_to_string(image)
 
-print(texto)
+print(texto,"\n\n")
 
---
+++ start.bat
@@ -0,0 +1,6 @@
+echo off
+echo Este script reconoce imagens con OCR
+:launch
+    python script.py
+    goto:launch
+pause
